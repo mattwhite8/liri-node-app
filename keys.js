5298192300c3748dd5cc3015e826dcf77215db97
Matt White <mattwhite1337@gmail.com>
2017-02-12 10:52:45 -0500

    initial commit

---
+++ keys.js
@@ -0,0 +1,8 @@
+console.log('this is loaded');
+
+exports.twitterKeys = {
+  consumer_key: 'WeFRYhRCgUfcAjVS82Zpud8A4',
+  consumer_secret: 'wKDM88eBfo1nfvqYD4PDEwsl3xk8j0VbyRsSeXsf1I9Hm2KYI6',
+  access_token_key: '830797310842003456-PhfkKzZz2PEkloFc3npAwPNZyd5bEvE',
+  access_token_secret: 'SBHgk5f9845rrYuRm3P4ySHGaIhuDgnpix10TlgEqtCJD',
+}
