c8b895320fb202a7a1f9dd9c4d0f9931325a6bf1
Matt White <mattwhite1337@gmail.com>
2017-02-12 13:04:41 -0500

    spotify integrated

--- liri.js
+++ liri.js
@@ -1,6 +1,9 @@
 var cmd = process.argv[2];
+var cmd2 = process.argv[3];
+
 var keys = require('./keys.js');
 var Twitter = require('twitter');
+var spotify = require('spotify');
 
 
 var client = new Twitter({
@@ -23,6 +26,40 @@ switch(cmd) {
 		}
 	})
 	break;
-	
+
+	case 'spotify-this-song':
+	if(cmd2){
+		spotify.search({type: 'track', query: cmd2}, function(error, data){
+			if(error) throw error;
+			console.log(data.tracks.items[0].album.artists[0].name);
+			console.log('=============');	
+			console.log(data.tracks.items[0].name);	
+			console.log('=============')
+			console.log(data.tracks.items[0].preview_url);
+			console.log('=============');
+			console.log(data.tracks.items[0].album.name);
+		})
+	}else {
+		spotify.lookup({type: 'track', id: '0hrBpAOgrt8RXigk83LLNE'}, function(error, data){
+			if(error) throw error;
+			console.log('');
+			console.log('You didn\'t put a song title in, so here you go:');
+			console.log('');
+			console.log(`Artist: ${data.album.artists[0].name}`);
+			console.log('=============');	
+			console.log(`Song title: ${data.name}`);	
+			console.log('=============')
+			console.log(`Preview URL: ${data.preview_url}`);
+			console.log('=============');
+			console.log(`Album: ${data.album.name}`);
+			console.log('');
+		})
+	}
+	break;
+
 }
+
+
+
+
 
