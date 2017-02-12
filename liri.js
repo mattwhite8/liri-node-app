72f26133d7df0f9c2bccb154f751436f85d8b8eb
Matt White <mattwhite1337@gmail.com>
2017-02-12 14:49:10 -0500

    added moviedb because omdb is down

--- liri.js
+++ liri.js
@@ -4,7 +4,7 @@ var cmd2 = process.argv[3];
 var keys = require('./keys.js');
 var Twitter = require('twitter');
 var spotify = require('spotify');
-
+var movieDB = require('moviedb')('cee1c5d1dfa35faf1c03da413bc82995');
 
 var client = new Twitter({
   consumer_key: keys.twitterKeys.consumer_key,
@@ -57,7 +57,44 @@ switch(cmd) {
 	}
 	break;
 
+	case 'movie-this':
+	if(cmd2){
+		movieDB.searchMovie({query: cmd2 }, function(error, data){
+			if(error) throw error;
+			console.log('');
+		  	console.log(`Title: ${data.results[0].title}`);
+		  	console.log('=============');
+		  	console.log(`Year: ${data.results[0].release_date}`);
+		  	console.log('=============');
+			console.log(`Rating: ${data.results[0].vote_average}`)
+			console.log('=============');  	
+			console.log(`Language: ${data.results[0].original_language}`);
+		  	console.log('=============');
+			console.log(`Plot: ${data.results[0].overview}`);
+			console.log('');		
+		});			
+	}else {
+		movieDB.searchTv({query: 'Mr. Robot' }, function(error, data){
+			if(error) throw error;
+			console.log('');
+			console.log('You didn\'t put a movie in, so here you go: ');
+			console.log('');
+		  	console.log(`Title: ${data.results[0].original_name}`);
+		  	console.log('=============');
+		  	console.log(`Year: ${data.results[0].first_air_date}`);
+		  	console.log('=============');
+			console.log(`Rating: ${data.results[0].vote_average}`)
+			console.log('=============');  	
+			console.log(`Language: ${data.results[0].original_language}`);
+		  	console.log('=============');
+			console.log(`Plot: ${data.results[0].overview}`);
+			console.log('');		
+		});
+	}
+	break;
+
 }
+
 
 
 
