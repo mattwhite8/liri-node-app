e871c65926ab9dc1706065b797aba93352d73d84
Matt White <mattwhite1337@gmail.com>
2017-02-15 21:01:03 -0500

    added log.txt

--- liri.js
+++ liri.js
@@ -18,10 +18,19 @@ function getTwitter(){
 	client.get('statuses/user_timeline',{screen_name: 'CLTWebDev', count: 20},function(error, tweets, response){
 		if(error) throw error;
 		for(var i = 0; i < tweets.length; i++){
+			console.log('');
 			console.log('================');
 			console.log(tweets[i].text);
 			console.log(tweets[i].created_at);
 			console.log('================');
+			console.log('');
+
+			writeLog('');
+			writeLog('================');
+			writeLog(tweets[i].text);
+			writeLog(tweets[i].created_at);
+			writeLog('================');
+			writeLog('');
 		}
 	})	
 }
@@ -30,6 +39,7 @@ function getSpotify(query){
 	if(cmd2){
 		spotify.search({type: 'track', query: query}, function(error, data){
 			if(error) throw error;
+			console.log('');
 			console.log(data.tracks.items[0].album.artists[0].name);
 			console.log('=============');	
 			console.log(data.tracks.items[0].name);	
@@ -37,6 +47,17 @@ function getSpotify(query){
 			console.log(data.tracks.items[0].preview_url);
 			console.log('=============');
 			console.log(data.tracks.items[0].album.name);
+			console.log('');
+
+			writeLog('');
+			writeLog(data.tracks.items[0].album.artists[0].name);
+			writeLog('=============')
+			writeLog(data.tracks.items[0].name);
+			writeLog('=============');
+			writeLog(data.tracks.items[0].preview_url);
+			writeLog('=============');
+			writeLog(data.tracks.items[0].album.name);
+			writeLog('');
 		})
 	}else {
 		spotify.lookup({type: 'track', id: '0hrBpAOgrt8RXigk83LLNE'}, function(error, data){
@@ -52,6 +73,18 @@ function getSpotify(query){
 			console.log('=============');
 			console.log(`Album: ${data.album.name}`);
 			console.log('');
+
+			writeLog('');
+			writeLog('You didn\'t put a song title in, so here you go:');
+			writeLog('');
+			writeLog(`Artist: ${data.album.artists[0].name}`);
+			writeLog('=============');	
+			writeLog(`Song title: ${data.name}`);	
+			writeLog('=============')
+			writeLog(`Preview URL: ${data.preview_url}`);
+			writeLog('=============');
+			writeLog(`Album: ${data.album.name}`);
+			writeLog('');
 		})
 	}	
 }
@@ -70,7 +103,19 @@ function getMovies(movie){
 			console.log(`Language: ${data.results[0].original_language}`);
 		  	console.log('=============');
 			console.log(`Plot: ${data.results[0].overview}`);
-			console.log('');		
+			console.log('');
+
+			writeLog('');
+		  	writeLog(`Title: ${data.results[0].title}`);
+		  	writeLog('=============');
+		  	writeLog(`Year: ${data.results[0].release_date}`);
+		  	writeLog('=============');
+			writeLog(`Rating: ${data.results[0].vote_average}`)
+			writeLog('=============');  	
+			writeLog(`Language: ${data.results[0].original_language}`);
+		  	writeLog('=============');
+			writeLog(`Plot: ${data.results[0].overview}`);
+			writeLog('');						
 		});			
 	}else {
 		movieDB.searchTv({query: 'Mr. Robot' }, function(error, data){
@@ -87,38 +132,74 @@ function getMovies(movie){
 			console.log(`Language: ${data.results[0].original_language}`);
 		  	console.log('=============');
 			console.log(`Plot: ${data.results[0].overview}`);
-			console.log('');		
+			console.log('');	
+
+			writeLog('');
+			writeLog('You didn\'t put a movie in, so here you go: ');
+			writeLog('');
+		  	writeLog(`Title: ${data.results[0].original_name}`);
+		  	writeLog('=============');
+		  	writeLog(`Year: ${data.results[0].first_air_date}`);
+		  	writeLog('=============');
+			writeLog(`Rating: ${data.results[0].vote_average}`)
+			writeLog('=============');  	
+			writeLog(`Language: ${data.results[0].original_language}`);
+		  	writeLog('=============');
+			writeLog(`Plot: ${data.results[0].overview}`);
+			writeLog('');				
 		});
 	}
+}
+
+function writeLog(data){
+	fs.appendFile('log.txt', data + '\n', 'UTF-8', function(error){
+		if (error) throw error;
+	})
 }
 
 switch(cmd) {
 
 	case 'my-tweets':
 		getTwitter();
+		writeLog('my-tweets');
 	break;
 
 	case 'spotify-this-song':
 		getSpotify(cmd2);
+		writeLog('spotify-this-song');
 	break;
 
 	case 'movie-this':
 		getMovies(cmd2);
+		writeLog('movie-this');
 	break;
 
 	case 'do-what-it-says':
-	fs.readFile("random.txt", "UTF-8", function(error, data){
-		if (error) throw error;
-		data = data.split(',');
+		fs.readFile("random.txt", "UTF-8", function(error, data){
+			if (error) throw error;
+			data = data.split(',');
 
-		if(data[0] === 'my-tweets'){
-			getTwitter();
-		}else if(data[0] === 'spotify-this-song'){
-			getSpotify(data[1]);
-		}else if(data[0] === 'movie-this'){
-			getMovies(data[1]);
-		}
-	})
+			if(data[0] === 'my-tweets'){
+				getTwitter();
+				writeLog('my-tweets');
+			}else if(data[0] === 'spotify-this-song'){
+				getSpotify(data[1]);
+				writeLog('spotify-this-song');
+			}else if(data[0] === 'movie-this'){
+				getMovies(data[1]);
+				writeLog('movie-this');
+			}
+		})
+	break;
+
+	default:
+		console.log('');
+		console.log("You didn\'t input a valid command");
+		console.log('');
+
+		writeLog('');
+		writeLog("You didn\'t input a valid command");
+		writeLog('');
 
 }
 
